8c12b9576cc90825d72a3ee151c6ffae59c12970
Nick Salvatore <nmsalvatore@gmail.com>
2024-04-04 11:29:11 -0700

    Change moderator_dashboard.html to volunteer_dashboard.html

--- accounts/views/dashboard.py
+++ accounts/views/dashboard.py
@@ -6,6 +6,6 @@ def dashboard_view(request):
         return render(request, 'superuser_dashboard.html')
     
     if request.user.is_authenticated:
-        return render(request, 'moderator_dashboard.html')
+        return render(request, 'volunteer_dashboard.html')
     
     return redirect('login')
