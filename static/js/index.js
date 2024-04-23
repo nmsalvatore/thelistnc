c29c8ca751c66d41c1cad5fefb737e251e994c25
Nick Salvatore <nmsalvatore@gmail.com>
2024-04-23 09:37:21 -0700

    Add confirm popups to update and delete operations

---
+++ static/js/index.js
@@ -0,0 +1,7 @@
+function confirmDelete() {
+    return confirm('Are you sure that you want to delete this event?\n\nThis operation cannot be reversed.')
+}
+
+function confirmUpdate() {
+    return confirm('Are you sure that you want to update this event?\n\nThis operation will overwrite pre-existing data.')
+}
