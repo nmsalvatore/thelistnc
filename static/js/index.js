436aec5507f634fa3de6fbb71b5165bd35775f95
Nick Salvatore <nmsalvatore@gmail.com>
2024-04-25 09:46:53 -0700

    Add alert if user is going to create multiple listings

--- static/js/index.js
+++ static/js/index.js
@@ -8,6 +8,45 @@ function confirmUpdate() {
     return confirm('Are you sure that you want to update this event?\n\nThis operation will overwrite pre-existing data.')
 }
 
+
+// alert user of multiple listing creation if end date exists
+
+function checkForMultipleListings() {
+    const endDateField = document.getElementById('id_end_date');
+    const endDateValue = endDateField.value;
+
+    if (endDateValue) {
+        const startDateField = document.getElementById('id_start_date');
+        const startDateValue = startDateField.value;
+
+        const startDate = new Date(formatDate(startDateValue));
+        const endDate = new Date(formatDate(endDateValue));
+        const allDates = getAllDates(startDate, endDate);
+        const allDatesFormatted = allDates.join('\n')
+
+        return confirm(`You are about to create identical listings for the following dates:\n\n${allDatesFormatted}\n\nClick OK if you wish to proceed.`);
+    }
+
+    function getAllDates(startDate, endDate) {
+        const allDates = []
+
+        let currentDate = startDate;
+        
+        while (currentDate <= endDate) {
+            allDates.push(currentDate.toLocaleDateString());
+            currentDate.setDate(currentDate.getDate() + 1)
+        }
+
+        return allDates;
+    }
+
+    function formatDate(date) {
+        let [year, month, day] = date.split('-');
+        return `${month}-${day}-${year}`
+    }
+}
+
+
 // listeners
 
 document.addEventListener('DOMContentLoaded', function() {
