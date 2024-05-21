1a8a5116fa775158f313159ba6622b4533c3d883
Nick Salvatore <nmsalvatore@gmail.com>
2024-05-21 14:55:55 -0700

    Convert venues and titles to lowercase in event ordering

--- events/views.py
+++ events/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.db.models.functions import Lower
+
 from .models import Event
 
 import datetime
@@ -6,7 +8,7 @@ import datetime
 
 def the_list(request, sorting='by-date'):
     if sorting == 'by-date':
-        grouped_events = group_events('start_date')
+        grouped_events = group_events('date')
         context = {'grouped_events': grouped_events}
         return render(request, 'events/event_list_by_date.html', context)
 
@@ -16,29 +18,35 @@ def the_list(request, sorting='by-date'):
         return render(request, 'events/event_list_by_venue.html', context)
 
     elif sorting == 'by-title':
-        events = Event.objects.filter(start_date__gte=datetime.date.today())
-        events = events.order_by('title', 'start_date')
-        context = {'events': events}
+        grouped_events = group_events('title')
+        context = {'grouped_events': grouped_events}
         return render(request, 'events/event_list_by_title.html', context)
 
 
 def group_events(field):
     events = Event.objects.filter(start_date__gte=datetime.date.today())
-    events = events.order_by(field)
-
     grouped_events = []
 
-    if field == 'start_date':
+    if field == 'date':
+        events = events.order_by('start_date')
         dates = events.dates('start_date', 'day')
         for date in dates:
             date_events = events.filter(start_date=date).order_by('start_time', 'end_time')
             grouped_events.append((date, date_events))
 
     if field == 'venue':
+        events = events.order_by(Lower('venue'))
         venues = events.values_list('venue', 'city').distinct()
         for venue_data in venues:
             venue = f'{venue_data[0]}, {venue_data[1]}'
             venue_events = events.filter(venue=venue_data[0], city=venue_data[1]).order_by('start_date', 'start_time', 'end_time')
             grouped_events.append((venue, venue_events))
+
+    if field == 'title':
+        events = events.order_by(Lower('title'))
+        titles = events.values_list('title', flat=True).distinct()
+        for title in titles:
+            title_events = events.filter(title=title).order_by('start_date', 'start_time', 'end_time')
+            grouped_events.append((title, title_events))
 
     return grouped_events
