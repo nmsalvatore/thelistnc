94b634b12f3d2f12551862624e47b0fb71b76440
Nick Salvatore <nmsalvatore@gmail.com>
2024-05-22 14:11:15 -0700

    Add event title to delete confirmation text

--- static/js/index.js
+++ static/js/index.js
@@ -1,73 +1,79 @@
 // confirmations
 
 function confirmDelete() {
-    return confirm('Are you sure that you want to delete this event?\n\nThis operation cannot be reversed.')
+    const titleElement = document.getElementById("id_title");
+    const title = titleElement.value;
+    return confirm(
+        `Are you sure that you want to delete the following event:\n\n${title}\n\nThis operation cannot be reversed.`,
+    );
 }
 
 function confirmUpdate() {
-    return confirm('Are you sure that you want to update this event?\n\nThis operation will overwrite pre-existing data.')
+    return confirm(
+        "Are you sure that you want to update this event?\n\nThis operation will overwrite pre-existing data.",
+    );
 }
-
 
 // alert user of multiple listing creation if end date exists
 
 function checkForMultipleListings() {
-    const endDateField = document.getElementById('id_end_date');
+    const endDateField = document.getElementById("id_end_date");
     const endDateValue = endDateField.value;
 
     if (endDateValue) {
-        const startDateField = document.getElementById('id_start_date');
+        const startDateField = document.getElementById("id_start_date");
         const startDateValue = startDateField.value;
 
         const startDate = new Date(formatDate(startDateValue));
         const endDate = new Date(formatDate(endDateValue));
         const allDates = getAllDates(startDate, endDate);
-        const allDatesFormatted = allDates.join('\n')
+        const allDatesFormatted = allDates.join("\n");
 
-        return confirm(`You are about to create identical listings for the following dates:\n\n${allDatesFormatted}\n\nClick OK if you wish to proceed.`);
+        return confirm(
+            `You are about to create identical listings for the following dates:\n\n${allDatesFormatted}\n\nClick OK if you wish to proceed.`,
+        );
     }
 
     function getAllDates(startDate, endDate) {
-        const allDates = []
+        const allDates = [];
 
         let currentDate = startDate;
-        
+
         while (currentDate <= endDate) {
             allDates.push(currentDate.toLocaleDateString());
-            currentDate.setDate(currentDate.getDate() + 1)
+            currentDate.setDate(currentDate.getDate() + 1);
         }
 
         return allDates;
     }
 
     function formatDate(date) {
-        let [year, month, day] = date.split('-');
-        return `${month}-${day}-${year}`
+        let [year, month, day] = date.split("-");
+        return `${month}-${day}-${year}`;
     }
 }
 
-
 // listeners
 
-document.addEventListener('DOMContentLoaded', function() {
-    const checkbox = document.getElementById('id_continuous');
-    const endDateField = document.getElementById('id_end_date');
+document.addEventListener("DOMContentLoaded", function () {
+    const checkbox = document.getElementById("id_continuous");
+    const endDateField = document.getElementById("id_end_date");
 
     if (checkbox && endDateField) {
         const endDateContainer = endDateField.parentElement;
         const isChecked = checkbox.checked;
-        toggleEndDateDisplay(isChecked)
-    
-        checkbox.addEventListener('click', () => {
+        toggleEndDateDisplay(isChecked);
+
+        checkbox.addEventListener("click", () => {
             const isChecked = checkbox.checked;
-            toggleEndDateDisplay(isChecked)
+            toggleEndDateDisplay(isChecked);
         });
 
         function toggleEndDateDisplay(isChecked) {
             if (isChecked) {
-                endDateContainer.style.display = 'flex';
+                endDateContainer.style.display = "flex";
             } else {
-                endDateContainer.removeAttribute('style');
+                endDateContainer.removeAttribute("style");
             }
         }
     }
