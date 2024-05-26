b3f5c9a11fed1525064c1c88b6e6d3c5ce441029
Nick Salvatore <nmsalvatore@gmail.com>
2024-05-26 09:20:21 -0700

    Add redirect to by-date event list on root url

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from events.views import the_list
-from .views import RobotsTxtView
+from .views import home, RobotsTxtView
 
 
 urlpatterns = [
-    path('', the_list, name='home'),
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('events/', include('events.urls')),
--- config/views.py
+++ config/views.py
@@ -1,4 +1,9 @@
+from django.shortcuts import redirect
 from django.views.generic import TemplateView
+
+def home(request):
+    return redirect('the_list', sorting='by-date')
+
 
 class RobotsTxtView(TemplateView):
     template_name = 'robots.txt'
