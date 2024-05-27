8792d1a49e50affeda78744a2f41982dd02164a7
Nick Salvatore <nmsalvatore@gmail.com>
2024-05-27 09:25:35 -0700

    Use natural sorting for venues and titles

--- events/views.py
+++ events/views.py
@@ -37,7 +37,8 @@ def group_events(field):
     if field == 'venue':
         events = events.order_by(Lower('venue'))
         venues = events.values_list('venue', 'city').distinct()
-        for venue_data in venues:
+        sorted_venues = sorted(venues, key=natural_sort_key)
+        for venue_data in sorted_venues:
             venue = f'{venue_data[0]}, {venue_data[1]}'
             venue_events = events.filter(venue=venue_data[0], city=venue_data[1]).order_by('start_date', 'start_time', 'end_time')
             grouped_events.append((venue, venue_events))
@@ -45,8 +46,18 @@ def group_events(field):
     if field == 'title':
         events = events.order_by(Lower('title'))
         titles = events.values_list('title', flat=True).distinct()
-        for title in titles:
+        sorted_titles = sorted(titles, key=natural_sort_key)
+        for title in sorted_titles:
             title_events = events.filter(title=title).order_by('start_date', 'start_time', 'end_time')
             grouped_events.append((title, title_events))
 
     return grouped_events
+
+
+def natural_sort_key(value):
+    name = value[0].lower() if isinstance(value, tuple) else value.lower()
+    articles = {'the', 'a', 'an'}
+    for article in articles:
+        if name.startswith(article + ' '):
+            name = name[len(article)+1:]
+    return (name, value[1]) if isinstance(value, tuple) else name
