2eb75c40d5ed3c9cedc67b14d0abd935cb213f56
Nick Salvatore <nmsalvatore@gmail.com>
2024-07-15 11:43:32 -0700

    Broke up register.py into smaller functions

--- accounts/views/register.py
+++ accounts/views/register.py
@@ -17,129 +17,121 @@ def register_view(request):
     """
     Handle user registration via invite
     """
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RegistrationForm(request.POST)
+
         if form.is_valid():
-            email = form.cleaned_data['email']
+            email = form.cleaned_data["email"]
             invitation = request.invitation
 
-            if invitation.email == email:
-                otp = str(random.randint(100000, 999999))
+            if invitation.email != email:
+                form.add_error("email", "Invalid email for the given invitation code.")
+                return render(request, "accounts/register.html", {"form": form})
 
-                # Initialize session data
-                request.session['email'] = email
-                request.session['otp'] = otp
-                request.session['otp_timestamp'] = time.time()
-                request.session['invitation_id'] = invitation.id
-                request.session['attempt_count'] = 0
+            otp = str(random.randint(100000, 999999))
+            request.session["email"] = email
+            request.session["otp"] = otp
+            request.session["otp_timestamp"] = time.time()
+            request.session["invitation_id"] = invitation.id
+            request.session["attempt_count"] = 0
 
-                # Send OTP via email
-                send_mail(
-                    'Your one-time passcode',
-                    f'Your one-time passcode is: {otp}',
-                    config('EMAIL_VERIFY_SENDER'),
-                    [email],
-                    fail_silently=False,
-                )
+            send_mail(
+                "Your one-time passcode",
+                f"Your one-time passcode is: {otp}",
+                config('EMAIL_USER'),
+                [email],
+                fail_silently=False,
+            )
 
-                return redirect('verify_register_otp')
-            else:
-                form.add_error('email', 'Invalid email for the given invitation code.')
-    else:
-        form = RegistrationForm()
+            return redirect("verify_register_otp")
 
-    return render(request, 'accounts/register.html', {'form': form})
+    form = RegistrationForm()
+    return render(request, "accounts/register.html", {"form": form})
 
 
 def verify_register_otp(request):
     """
     Complete registration with one-time passcode
     """
+    email = request.session.get("email")
+    otp_timestamp = request.session.get("otp_timestamp")
+    time_elapsed = time.time() - otp_timestamp
+    attempt_count = request.session.get("attempt_count")
+    verify_template = "accounts/verify_otp.html"
 
-    # Retrieve session data
-    email = request.session.get('email')
-    stored_otp = request.session.get('otp')
-    otp_timestamp = request.session.get('otp_timestamp')
-    attempt_count = request.session.get('attempt_count')
+    if request.method == "GET":
+        context = {"email": email}
+        return render(request, verify_template, context)
 
-    # GET request logic
-    if request.method == 'GET':
-        context = {
-            'email': email,
-        }
-        return render(request, 'accounts/verify_otp.html', {'email': email})
-
-    # POST request logic
-    if request.method == 'POST':
-
-        # Increment attempt count and save to session
+    if request.method == "POST":
         attempt_count += 1
-        request.session['attempt_count'] = attempt_count
+        request.session["attempt_count"] = attempt_count
+        context = {"email": email}
 
-        # Retrieve form OTP value
-        otp = request.POST.get('otp')
+        if time_elapsed > 300:
+            error_message = "Passcode has expired."
+            context["error_message"] = error_message
+            return render(request, verify_template, context)
 
-        # Retrieve invitation ID
-        invitation_id = request.session.get('invitation_id')
+        if attempt_count == 3:
+            error_message = "Invalid passcode. Attempt limit has been reached."
+            context["error_message"] = error_message
+            return render(request, verify_template, context)
 
-        # If attempts exceed 3, throw error
         if attempt_count > 3:
-            error_message = 'Attempt limit has been exceeded.'
-            context = {
-                'email': email,
-                'error_message': error_message,
-            }
-            return render(request, 'accounts/verify_otp.html', context)
+            error_message = "Attempt limit has been exceeded."
+            context["error_message"] = error_message
+            return render(request, verify_template, context)
 
-        # Check for matching OTP values
-        if otp and stored_otp and otp == stored_otp:
+        otp = request.POST.get("otp")
+        if not otp:
+            error_message = "Please enter a valid passcode"
+            context["error_message"] = error_message
+            return render(request, verify_template, context)
 
-            # If OTP values match and 5 minutes has not passed, create and log in user
-            if time.time() - otp_timestamp <= 300:
+        stored_otp = request.session.get("otp")
+        if not stored_otp:
+            error_message = "Something went wrong."
+            context["error_message"] = error_message
+            return render(request, verify_template, context)
 
-                # Add user to database
-                user = User.objects.create_user(username=email, email=email)
-                UserProfile.objects.create(user=user)
+        if otp != stored_otp:
+            error_message = "Invalid passcode."
+            context["error_message"] = error_message
+            return render(request, verify_template, context)
 
-                # Mark user invitation as used
-                invitation = Invitation.objects.get(id=invitation_id)
-                invitation.is_used = True
-                invitation.save()
+        user = create_user(email)
+        use_invitation(request)
+        delete_session_data(request)
+        login(request, user)
+        return redirect("registration_success")
 
-                # Delete session data
-                del request.session['email']
-                del request.session['otp']
-                del request.session['otp_timestamp']
-                del request.session['invitation_id']
-                del request.session['attempt_count']
-
-                # Log in user
-                login(request, user)
-
-                return redirect('registration_success')
-
-            # If stored OTP has expired, throw error
-            else:
-                error_message = 'Passcode has expired.'
-                context = {
-                    'email': email,
-                    'error_message': error_message,
-                }
-                return render(request, 'accounts/verify_otp.html', context)
-
-        # If OTP values do not match, throw error
-        else:
-            error_message = 'Invalid passcode.'
-            context = {
-                'email': email,
-                'error_message': error_message,
-            }
-            return render(request, 'accounts/verify_otp.html', context)
-
-    # If request is not POST or GET, redirect to home
-    else:
-        return redirect('home')
+    return redirect("home")
 
 
 def registration_success(request):
-    return render(request, 'accounts/registration_success.html')
+    if request.user.is_authenticated:
+        return render(request, "accounts/registration_success.html")
+    else:
+        return redirect("home")
+
+
+def delete_session_data(request):
+    del request.session["email"]
+    del request.session["otp"]
+    del request.session["otp_timestamp"]
+    del request.session["invitation_id"]
+    del request.session["attempt_count"]
+
+
+def create_user(email):
+    user = User.objects.create_user(username=email, email=email)
+    UserProfile.objects.create(user=user)
+    return user
+
+
+def use_invitation(request):
+    invitation_id = request.session.get("invitation_id")
+    invitation = Invitation.objects.get(id=invitation_id)
+    invitation.is_used = True
+    invitation.save()
