e778104dd9c79a4717dcf28eba2ae95ee04bd917
Nick Salvatore <nmsalvatore@gmail.com>
2024-07-15 10:12:03 -0700

    Break up invitation.py into smaller functions; Modify error message

--- accounts/views/invitation.py
+++ accounts/views/invitation.py
@@ -4,58 +4,54 @@ from django.contrib.auth.decorators import user_passes_test
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from decouple import config
-
 from accounts.models import Invitation
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def send_invitation(request):
     if request.method == 'POST':
-        # Get email from form
         email = request.POST['email']
 
-        # Check for previous invitations
         already_invited = Invitation.objects.filter(email=email)
-        if already_invited:
+        if already_invited == None:
             error_message = 'User has already received an invitation.'
             return render(request, 'accounts/send_invitation.html', {'error_message': error_message})
 
-        # Create invitation
         invitation = Invitation.objects.create(email=email)
-
-        # Configure email for Mailgun SMTP
-        subject = 'You\'ve been invited to be a volunteer for The List NC'
-        base_url = config('BASE_URL')
-        registration_url = f'{base_url}/accounts/register?code={invitation.code}'
-        context = {
-            'email': email,
-            'registration_url': registration_url,
-        }
-        html_message = render_to_string('accounts/invitation_email.html', context)
-        plain_message = strip_tags(html_message)
-        from_email = config('EMAIL_INVITE_SENDER')
-        recipient_list = [email]
-
-        # Send email using Mailgun SMTP
         try:
-            send_mail(
-                subject,
-                plain_message,
-                from_email,
-                recipient_list,
-                html_message=html_message,
-                fail_silently=False,
-            )
+            send_invitation_email(email, invitation)
         except:
-            error_message = 'User email has not been verified with Mailgun.'
+            error_message = 'Something went wrong.'
             invitation.delete()
             return render(request, 'accounts/send_invitation.html', {'error_message': error_message})
 
         return redirect('invitation_sent')
-
     return render(request, 'accounts/send_invitation.html')
 
 
 @user_passes_test(lambda u: u.is_superuser)
 def invitation_sent(request):
     return render(request, 'accounts/invitation_sent.html')
+
+
+def send_invitation_email(recipient_email, invitation):
+    subject = 'You\'ve been invited to be a volunteer for The List NC'
+    base_url = config('BASE_URL')
+    registration_url = f'{base_url}/accounts/register?code={invitation.code}'
+    context = {
+        'email': recipient_email,
+        'registration_url': registration_url,
+    }
+    html_message = render_to_string('accounts/invitation_email.html', context)
+    plain_message = strip_tags(html_message)
+    from_email = config('EMAIL_USER')
+    recipient_list = [recipient_email]
+
+    send_mail(
+        subject,
+        plain_message,
+        from_email,
+        recipient_list,
+        html_message=html_message,
+        fail_silently=False,
+    )
