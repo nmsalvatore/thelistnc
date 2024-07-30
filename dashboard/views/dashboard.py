5a94e1357279eeaf92e52f2ba20886fbc58a1088
Nick Salvatore <nmsalvatore@gmail.com>
2024-07-29 21:05:33 -0700

    Fix admin view with reverted list style

--- dashboard/views/dashboard.py
+++ dashboard/views/dashboard.py
@@ -8,6 +8,7 @@ import datetime
 
 def dashboard_view(request, sorting='by-date'):
     template = ''
+    context = {}
     grouped_events = []
 
     user = request.user
@@ -15,10 +16,13 @@ def dashboard_view(request, sorting='by-date'):
     if user.is_authenticated:
         if sorting == 'by-date':
             grouped_events = group_events('date')
+            context = {'grouped_events': grouped_events}
         elif sorting == 'by-venue':
             grouped_events = group_events('venue')
+            context = {'grouped_events': grouped_events}
         elif sorting == 'by-title':
-            grouped_events = group_events('title')
+            events = Event.objects.filter(start_date__gte=datetime.date.today()).order_by("title", "start_date", "start_time")
+            context = {'events': events}
 
         if user.is_superuser:
             if sorting == 'by-date':
@@ -36,7 +40,6 @@ def dashboard_view(request, sorting='by-date'):
             elif sorting == 'by-title':
                 template = 'dashboard/dashboard_volunteer_by_title.html'
 
-        context = {'grouped_events': grouped_events}
         return render(request, template, context)
 
     else:
