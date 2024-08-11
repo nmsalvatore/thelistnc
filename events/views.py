c6ccd85295b09ce986eac236dc57bfe8c78c2a64
Nick Salvatore <nmsalvatore@gmail.com>
2024-08-11 14:08:29 -0700

    Fun new UI for public views

--- events/views.py
+++ events/views.py
@@ -9,17 +9,22 @@ import datetime
 def the_list(request, sorting='by-date'):
     if sorting == 'by-date':
         grouped_events = group_events('date')
-        context = {'grouped_events': grouped_events}
+        dates = Event.objects.filter(start_date__gte=datetime.date.today()).values_list('start_date', flat=True).distinct().order_by('start_date')
+        context = {'grouped_events': grouped_events, 'dates': dates}
         return render(request, 'events/event_list_by_date.html', context)
 
     elif sorting == 'by-venue':
         grouped_events = group_events('venue')
-        context = {'grouped_events': grouped_events}
+        venues = Event.objects.filter(start_date__gte=datetime.date.today()).values_list('venue', flat=True).distinct().order_by('venue')
+        sorted_venues = sorted(venues, key=natural_sort_key)
+        context = {'grouped_events': grouped_events, 'venues': sorted_venues}
         return render(request, 'events/event_list_by_venue.html', context)
 
     elif sorting == 'by-title':
-        events = Event.objects.filter(start_date__gte=datetime.date.today()).order_by("title", "start_date", "start_time")
-        context = {'events': events}
+        grouped_events = group_events('title')
+        titles = Event.objects.filter(start_date__gte=datetime.date.today()).values_list('title', flat=True).distinct()
+        sorted_titles = sorted(titles, key=natural_sort_key)
+        context = {'grouped_events': grouped_events, 'titles': sorted_titles}
         return render(request, 'events/event_list_by_title.html', context)
 
 
@@ -32,16 +37,17 @@ def group_events(field):
         dates = events.dates('start_date', 'day')
         for date in dates:
             date_events = events.filter(start_date=date).order_by('start_time', 'end_time', 'title')
-            grouped_events.append((date, date_events))
+            num_events = len(date_events)
+            grouped_events.append((date, date_events, num_events))
 
     if field == 'venue':
         events = events.order_by(Lower('venue'))
-        venues = events.values_list('venue', 'city').distinct()
+        venues = events.values_list('venue', flat=True).distinct()
         sorted_venues = sorted(venues, key=natural_sort_key)
-        for venue_data in sorted_venues:
-            venue = f'{venue_data[0]}, {venue_data[1]}'
-            venue_events = events.filter(venue=venue_data[0], city=venue_data[1]).order_by('start_date', 'start_time', 'end_time')
-            grouped_events.append((venue, venue_events))
+        for venue in sorted_venues:
+            venue_events = events.filter(venue=venue).order_by('start_date', 'start_time', 'end_time')
+            num_events = len(venue_events)
+            grouped_events.append((venue, venue_events, num_events))
 
     if field == 'title':
         events = events.order_by(Lower('title'))
@@ -49,13 +55,14 @@ def group_events(field):
         sorted_titles = sorted(titles, key=natural_sort_key)
         for title in sorted_titles:
             title_events = events.filter(title=title).order_by('start_date', 'start_time', 'end_time')
-            grouped_events.append((title, title_events))
+            num_events = len(title_events)
+            grouped_events.append((title, title_events, num_events))
 
     return grouped_events
 
 
 def natural_sort_key(value):
-    name = value[0].lower() if isinstance(value, tuple) else value.lower()
+    name = value.lower() if isinstance(value, tuple) else value.lower()
     articles = {'the', 'a', 'an'}
     for article in articles:
         if name.startswith(article + ' '):
